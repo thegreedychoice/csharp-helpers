d1ad3c593892f937145b06de6bc435e2369ec6f0
Shubham Shukla <shubham.shukla@citrix.com>
2022-06-30 13:30:59 -0400

    Added some standard extension methods

---
+++ Extensions/ExtensionMethods/ExtensionMethods/ExtensionMethods.Library/ConfigurationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+
+namespace ExtensionMethods.Library
+{
+    public static class ConfigurationExtensions
+    {
+        public static bool IsLoaded(this IConfiguration config)
+        {
+            return config != null && config.AsEnumerable().Any();
+        }
+
+        public static IConfigurationBuilder AddStandardProviders(this IConfigurationBuilder configurationBuilder)
+        {
+            return configurationBuilder.AddJsonFile("appsettings.json")
+                                       .AddEnvironmentVariables()
+                                       .AddJsonFile("config/config.json", optional: true)
+                                       .AddJsonFile("secrets/secrets.json", optional: true);
+        }
+    } 
+}
+
---
+++ Extensions/ExtensionMethods/ExtensionMethods/ExtensionMethods.Tests/ConfigurationTests.cs
@@ -0,0 +1,23 @@
+﻿using ExtensionMethods.Library;
+using Microsoft.Extensions.Configuration;
+
+namespace ExtensionMethods.Tests;
+
+public class ConfigurationTests
+{
+    [Test]
+    public void IsLoaded()
+    {
+        IConfiguration config = null;
+        Assert.IsFalse(config.IsLoaded());
+    }
+
+    [Test]
+    public void AddStandardProviders()
+    {
+        var builder = new ConfigurationBuilder();
+        var config = builder.AddStandardProviders().Build();
+        Assert.AreEqual(4, config.Providers.Count());
+        Assert.IsTrue(config.IsLoaded());
+    }
+}
